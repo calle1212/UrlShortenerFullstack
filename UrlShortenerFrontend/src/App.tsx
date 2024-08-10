a7056ee151ce7561b70c6fb626fa8e3bafdf9902
calle1212 <carl.hasselberg.6020@student.uu.se>
2024-08-10 15:48:20 +0200

    feat: add interface and mockdata

--- UrlShortenerFrontend/src/App.tsx
+++ UrlShortenerFrontend/src/App.tsx
@@ -1,11 +1,54 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+
+interface UrlProps {
+  longUrl: string;
+  shortUrl: string;
+  timesUsed: number;
+}
+
+const mockUrlPropsArray: UrlProps[] = [{"longUrl": "https://www.google.se/", "shortUrl": "goog", "timesUsed": 0}, {"longUrl": "https://www.salt.dev/", "shortUrl": "salt", "timesUsed": 0}]
+
+function PostField() {
+  function handlePost() {
+    
+  }
+  return (
+    <div>
+  <input></input>
+  <button onClick={handlePost}>submit</button>
+  </div>
+)
+}
+
+function UrlBlocks(){
+  function getBlock(props:UrlProps){
+    return(
+      <div>
+        <p>{props.longUrl}</p>
+        <p>{props.shortUrl}</p>
+        <p>{props.timesUsed}</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      {mockUrlPropsArray.map(getBlock)}
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const urlPath = window.location.pathname;
+  if (urlPath == "/google") {
+    window.location.href = "http://google.se";
+    return (<></>)
+  }
   return (
     <>
-  <p>hejehej</p>
+      <h1>{urlPath}</h1>
+      <PostField/>
+      <p>hejehej</p>
+      <UrlBlocks/>
     </>
   )
 }
